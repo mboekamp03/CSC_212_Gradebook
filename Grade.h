81a7b14b07fbbbeb86b6f7457411f2d581763d2e
Matt Boekamp <98491428+mboekamp03@users.noreply.github.com>
2023-06-13 19:41:40 -0400

    Create Grade.h

---
+++ Grade.h
@@ -0,0 +1,47 @@
+// Grade.H
+
+// get all grades
+Class Grade {
+
+Private:
+  std::vector <double> labGrades;
+  std::vector <double> assingmentGrades;
+  std::vector <double> projectGrades;
+  double examGrade;
+
+  double totLab;
+
+  double totAssignment;
+
+  double totGrade;
+
+Public:
+
+  //pushback the value given into the vector of Lab grades
+
+  int getLabGrade(int gradeValue);
+
+  //pushback the value given into the vector of Assignment grades
+
+  int getAssignmentGrade(int gradeValue);
+
+  //pushback the value given into the vector of project grades
+
+  int getProjectGrade(int gradeValue);
+
+  //sets the value of private Final exam
+
+  int getFinalExamGrade(int gradeValue);
+
+  //uses private vector to get total lab grades using vec.size()
+
+  double setTotLab();
+
+  //uses private vector to get total Assignment grades using vec.size()
+
+  double setTotAssignment();
+
+  //uses private totlab and totAssignment along with vector for project grades
+
+  double totGrade();
+};
