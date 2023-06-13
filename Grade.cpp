22ed4f9550558288613f736f8cfc90318521594b
Matt Boekamp <98491428+mboekamp03@users.noreply.github.com>
2023-06-13 19:46:40 -0400

    Create Grade.cpp

---
+++ Grade.cpp
@@ -0,0 +1,48 @@
+// Grade.cpp
+#include "Grade.h"
+
+//pushback the value given into the vector of Lab grades 
+int Grade::getLabGrade(int gradeValue){ 
+  takes the grade value uses it to do labGrades.pushback(gradeValue)
+}//
+
+//pushback the value given into the vector of Assignment grades 
+int Grade::getAssignmentGrade(int gradeValue){ 
+  //takes the grade value uses it to do assignmentGrades.pushback(gradeValue) 
+}
+
+//pushback the value given into the vector of project grades 
+int Grade::getProjectGrade(int gradeValue) {
+  //takes the grade value uses it to do projectGrades.pushback(gradeValue)
+ }
+
+//sets the value of private Final exam
+int Grade::getFinalExamGrade(int gradeValue){ 
+  //uses this keyword to set private value of finalExam to gradeValue
+ }
+
+//uses private vector to get total lab grades using vec.size() 
+double Grade::setTotLab(){
+
+  //make a temp value
+  //use a for loop to go to the size of labGrades.size()
+	//add to temp value
+
+  //end for loop
+
+  //set totlab to temp
+}
+
+//uses private vector to get total Assignment grades using vec.size() 
+double Grade::setTotAssignment(){ 
+  //make a temp value use a for loop to go to the size of labGrades.size() add to temp value
+
+  //end for loop
+
+  //set totAssignment to temp
+}
+
+//uses private totlab and totAssignment along with vector for project grades
+double Grade::totGrade(){
+
+}
