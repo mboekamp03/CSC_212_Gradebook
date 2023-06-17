ae8da3e48fc8bc4ea44f5eba9d893dff75ec5128
Matt Boekamp <98491428+mboekamp03@users.noreply.github.com>
2023-06-17 16:56:18 -0400

    Update Grade.h

--- Grade.h
+++ Grade.h
@@ -1,39 +1,42 @@
 // Grade.H
+
 #include <iostream>
 #include <vector>
-
 // get all grades
 class Grade {
 
   private:
-    std::vector <double> labGrades;
-    std::vector <double> assingmentGrades;
-    std::vector <double> projectGrades;
+    struct Assignment {
+        std::string assignmentName;
+        int assignmentGrade;
+    };
+
+    std::vector <Assignment> labGrades;
+    std::vector <Assignment> assignmentGrades;
+    std::vector <Assignment> projectGrades;
     double examGrade;
-
+ 
     double totLab;
-
     double totAssignment;
-
     double totGrade;
 
   public:
 
     //pushback the value given into the vector of Lab grades
 
-    int getLabGrade(int gradeValue);
+    void getLabGrade(int gradeValue, std::string gradeName);
 
     //pushback the value given into the vector of Assignment grades
 
-    int getAssignmentGrade(int gradeValue);
+    void getAssignmentGrade(int gradeValue, std::string gradeName);
 
     //pushback the value given into the vector of project grades
 
-    int getProjectGrade(int gradeValue);
+    void getProjectGrade(int gradeValue, std::string gradeName);
 
     //sets the value of private Final exam
 
-    int getFinalExamGrade(int gradeValue);
+    void getFinalExamGrade(int gradeValue);
 
     //uses private vector to get total lab grades using vec.size()
 
