2cdd0594201bfa5940a3d5452100b4223e312e86
Matt Boekamp <98491428+mboekamp03@users.noreply.github.com>
2023-06-21 21:10:59 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "Grade.h"
+
+int main(int argc, char*argv[]) {
+    std::string fileName = argv[1];
+    std::ifstream file(fileName);
+    std::string line;
+    
+    while()
+
+    
+    return 0;
+}
