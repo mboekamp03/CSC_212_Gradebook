66cd51e34df96a501f19d185594cf5897ca9bec6
JosephYan-1 <113352554+JosephYan-1@users.noreply.github.com>
2023-06-21 21:42:58 -0400

    Update Grade.cpp

--- Grade.cpp
+++ Grade.cpp
@@ -1,74 +1,76 @@
 // Grade.cpp
 #include "Grade.h"
 
-//pushback the value given into the vector of Lab grades 
-void Grade::getLabGrade(int gradeValue, std::string gradeName){ 
-  //takes the grade value uses it to do labGrades.pushback(gradeValue)
-  Assignment lab;
-  lab.assignmentGrade = gradeValue;
-  lab.assignmentName = gradeName;
-  projectGrades.push_back(lab);
+//pushback the value given into the vector of Lab grades
+void Grade::getLabGrade(int gradeValue, std::string gradeName){
+    //takes the grade value uses it to do labGrades.pushback(gradeValue)
+    Assignment lab;
+    lab.assignmentGrade = gradeValue;
+    lab.assignmentName = gradeName;
+    projectGrades.push_back(lab);
 }
 
-//pushback the value given into the vector of Assignment grades 
-void Grade::getAssignmentGrade(int gradeValue, std::string gradeName){ 
-  //takes the grade value uses it to do assignmentGrades.pushback(gradeValue) 
-  Assignment assignment;
-  assignment.assignmentGrade = gradeValue;
-  assignment.assignmentName = gradeName;
-  assignmentGrades.push_back(assignment);
-  
+//pushback the value given into the vector of Assignment grades
+void Grade::getAssignmentGrade(int gradeValue, std::string gradeName){
+    //takes the grade value uses it to do assignmentGrades.pushback(gradeValue)
+    Assignment assignment;
+    assignment.assignmentGrade = gradeValue;
+    assignment.assignmentName = gradeName;
+    assignmentGrades.push_back(assignment);
+
 }
 
-//pushback the value given into the vector of project grades 
+//pushback the value given into the vector of project grades
 void Grade::getProjectGrade(int gradeValue, std::string gradeName) {
-  //takes the grade value uses it to do projectGrades.pushback(gradeValue)
-  Assignment project;
-  project.assignmentGrade = gradeValue;
-  project.assignmentName = gradeName;
-  projectGrades.push_back(project);
+    //takes the grade value uses it to do projectGrades.pushback(gradeValue)
+    Assignment project;
+    project.assignmentGrade = gradeValue;
+    project.assignmentName = gradeName;
+    projectGrades.push_back(project);
 
 }
 
 //sets the value of private Final exam
-void Grade::getFinalExamGrade(int gradeValue){ 
-  //uses this keyword to set private value of finalExam to gradeValue
-  this->examGrade = gradeValue;
+void Grade::getFinalExamGrade(int gradeValue){
+    //uses this keyword to set private value of finalExam to gradeValue
+    this->examGrade = gradeValue;
 }
 
 //uses private vector to get total lab grades using vec.size()
 double Grade::setTotLab(){
-  //Initialize sum and totalPoints, which will store the total points earned and total points possible respectively
-  int sum = 0;
-  int totLab = 0;
+    //Initialize sum and totalPoints, which will store the total points earned and total points possible respectively
+    int sum = 0;
+    int totLab = 0;
 
 
-  //Loops through assignment vector, adds the points to sum, and total possible points to totalPoints after the loop ends
-  for(int i = 0; i < labGrades.size(); i++){
-    int currentGrade = labGrades[i].assignmentGrade;
-    sum = currentGrade + sum;
+    //Loops through assignment vector, adds the points to sum, and total possible points to totalPoints after the loop ends
+    for(int i = 0; i < labGrades.size(); i++){
+        int currentGrade = labGrades[i].assignmentGrade;
+        sum = currentGrade + sum;
 
-  }
+    }
 
-  totLab = sum;
-  return totLab;
+    totLab = sum;
+    return totLab;
 }
 
-//uses private vector to get total Assignment grades using vec.size() 
-double Grade::setTotAssignment(){ 
-  int sum = 0;
-  int totAssignment = 0;
+//uses private vector to get total Assignment grades using vec.size()
+double Grade::setTotAssignment(){
+    int sum = 0;
+    int totAssignment = 0;
 
 
     //Loops through assignment vector, adds the points to sum, and total possible points to totalPoints after the loop ends
     for(int i = 0; i < assignmentGrades.size(); i++){
-      sum = sum + assignmentGrades[i].assignmentGrade;
+        sum = sum + assignmentGrades[i].assignmentGrade;
     }
 
-  totAssignment = sum;
-  return totAssignment;
+    totAssignment = sum;
+    return totAssignment;
 }
-
+void Grade::setStudentName(std::string name){
+    this->studentName = name;
+}
 //uses private totlab and totAssignment along with vector for project grades
 double Grade::setTotGrade(){
 
@@ -78,8 +80,8 @@ double Grade::setTotGrade(){
 
 
     //Loops through assignment vector, adds the points to sum, and total possible points to totalPoints after the loop ends
-     for(int i = 0; i<assignmentGrades.size(); i++){
-         sum = assignmentGrades[i].assignmentGrade + sum;
+    for(int i = 0; i<assignmentGrades.size(); i++){
+        sum = assignmentGrades[i].assignmentGrade + sum;
 
     }
     totalPoints = assignmentGrades.size() * 50;
@@ -96,7 +98,7 @@ double Grade::setTotGrade(){
 
 
     //Converts the project grade percent to its weight in points, adds it to the sum and total possible points to totalPoints
-     for(int i = 1;i <= projectGrades.size(); i++){
+    for(int i = 1;i <= projectGrades.size(); i++){
         //labGrades[i] = 5;
         sum = sum + projectGrades[i].assignmentGrade;
     }
@@ -153,7 +155,7 @@ double Grade::getIndividualGrade(std::string gradeCategory, std::string gradeNam
         }
     }
     else {
-       return 0;
+        return 0;
     }
-  return foundGrade;
+    return foundGrade;
 }
