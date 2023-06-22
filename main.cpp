9fb302659add4f215c8f02cab06ae71e8d12fe28
JosephYan-1 <113352554+JosephYan-1@users.noreply.github.com>
2023-06-21 21:43:31 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,36 +6,40 @@
 #include "Grade.h"
 
 int main(int argc, char*argv[]) {
+    Grade student;
     std::string fileName = argv[1];
     std::ifstream file(fileName);
     std::string line;
-    
+    std::getline(file, line);
+    student.setStudentName(line);
+
+
     std::string category;
     std::string name;
     double grade;
 
     while(std::getline(file, line)) {
         std::istringstream data(line);
-        
+
         //spliting data into category, name, grade
-        
+
         data >> category >> name >> grade;
 
         //testing to see what the category is so we can call the right function
-        
-        if (category == 'Lab' || category == 'lab'){
-            getLabGrade(grade, name);
+
+        if (category == "Lab" || category == "lab"){
+            student.getLabGrade(grade, name);
         }
-        else if (category == 'Assignment' || category == 'assignment'){
-            getAssignmentGrade(grade, name);
+        else if (category == "Assignment" || category == "assignment"){
+            student.getAssignmentGrade(grade, name);
         }
-        else if (category == 'Project' || category == 'project'){
-            getProjectGrade(grade, name);
+        else if (category == "Project" || category == "project"){
+            student.getProjectGrade(grade, name);
         }
         else{
-            getFinalExamGrade(grade);
+            student.getFinalExamGrade(grade);
         }
     }
-    
+
     return 0;
 }
